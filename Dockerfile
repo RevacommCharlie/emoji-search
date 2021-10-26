ca482ba4c9c0194377126114069407be2f183247
Charlie Sakamaki <csakamaki@revacomm.com>
2021-10-25 15:59:18 -1000

    Initial port from fork

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM registry1.dso.mil/ironbank/opensource/nodejs/nodejs16:16.5.0 AS builder
+# FROM node AS builder
+
+USER node
+
+WORKDIR /home/node
+COPY --chown=node:node . .
+RUN npm ci
+RUN npm run build
+
+# Stage 2
+# FROM registry.il2.dso.mil/platform-one/devops/pipeline-templates/base-image/harden-nginx-20:1.20.1
+# USER appuser
+# COPY --from=builder --chown=appuser:appuser /home/node/build /var/www
+
+FROM registry1.dso.mil/ironbank/opensource/nginx/nginx:1.21.1
+USER nginx
+COPY --from=builder --chown=nginx:nginx /home/node/build /etc/nginx/html
+
+# FROM nginx
+# COPY --from=builder --chown=nginx:nginx /home/node/build /usr/share/nginx/html
+# EXPOSE 80
+
+EXPOSE 8080
+
+ENTRYPOINT [ "nginx",  "-g", "daemon off;" ]
--- src/Header.js
+++ src/Header.js
@@ -4,21 +4,23 @@ import "./Header.css";
 export default class Header extends PureComponent {
   render() {
     return (
-      <header className="component-header">
-        <img
-          src="//cdn.jsdelivr.net/emojione/assets/png/1f638.png"
-          width="32"
-          height="32"
-          alt=""
-        />
-        Emoji Search
-        <img
-          src="//cdn.jsdelivr.net/emojione/assets/png/1f63a.png"
-          width="32"
-          height="32"
-          alt=""
-        />
-      </header>
+      <div>
+        <header className="component-header">
+          <img
+            src="//cdn.jsdelivr.net/emojione/assets/png/1f638.png"
+            width="32"
+            height="32"
+            alt=""
+          />
+          Example Application: Emoji Search
+          <img
+            src="//cdn.jsdelivr.net/emojione/assets/png/1f63a.png"
+            width="32"
+            height="32"
+            alt=""
+          />
+        </header>
+      </div>
     );
   }
 }
